69bc58427680115a9b1e05acef8b7ad756b98ecc
Hugo Clio <clio@mini-3.local>
2017-02-04 15:28:51 +0000

    validacoes mensagens

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,5 +1,5 @@
 class Message < ActiveRecord::Base
 
-	#validates_inclusion_of :tipo, :in => [true, false]
-	# validates :title, :body, :presence => true
+	validates_inclusion_of :tipo, :in => [true, false]
+    validates :title, :body, :presence => true
 end
